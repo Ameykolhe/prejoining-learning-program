2dbb52c36589b68c689d2b883235f8181780806f
Amey Kolhe <kolheamey99@gmail.com>
2020-04-29 18:16:33 +0530

    week4-java-assignment2 Complete

---
+++ Week4/week4-java-assignment2/src/com/sapient/week4/exceptions/AdditionException.java
@@ -0,0 +1,12 @@
+package com.sapient.week4.exceptions;
+
+public class AdditionException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5016235759666233480L;
+
+	public AdditionException(String s) {
+		super(s);
+	}
+}
---
+++ Week4/week4-java-assignment2/src/com/sapient/week4/exceptions/MultiplicationException.java
@@ -0,0 +1,12 @@
+package com.sapient.week4.exceptions;
+
+public class MultiplicationException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6075287602471647202L;
+
+	public MultiplicationException(String s) {
+		super(s);
+	}
+}
